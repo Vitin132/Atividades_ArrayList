ffe9634ed42b89d20ba3c0d47ecb7e7e9fc414ec
Vitor Lopes <86317769+Vitin132@users.noreply.github.com>
2025-03-17 19:40:15 -0300

    Alterações

--- AT8_ListaDePaises.java
+++ AT8_ListaDePaises.java
@@ -1,5 +1,3 @@
-
-
 package Atividades_ArrayList;
 
 import java.util.ArrayList;
@@ -12,29 +10,29 @@ import java.util.Scanner;
  */
 public class AT8_ListaDePaises {
 
-    
     public static void main(String[] args) {
-        
+
         ArrayList<String> paises = new ArrayList<>();
         Collections.addAll(paises, "Brasil", "Portugal", "EUA", "Canadá", "Rússia");
         Scanner tc = new Scanner(System.in);
-        
+
         System.out.println("Lista de países:");
         System.out.println(paises);
-        
-        for(int i = 0; i < paises.size();  i++){
-            
-            System.out.println("Digite o nome de outros países: ");
+
+        for (int i = 0; i < paises.size(); i++) {
+
+            System.out.println("Digite o nome de outros países: (para parar digite 'n') ");
             String pais = tc.nextLine();
             paises.add(pais);
-            
+
             System.out.println(paises);
-            
-            
+            if(pais.equalsIgnoreCase("n")){
+                
+                break;
+            }
+
         }
-        
-        
+
     }
-    
-    
+
 }
