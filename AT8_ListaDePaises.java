8bb980400aaf546a2276ef83bbc67ad121d07829
Vitor Lopes <86317769+Vitin132@users.noreply.github.com>
2025-03-17 19:25:53 -0300

    17-03-2025

--- AT8_ListaDePaises.java
+++ AT8_ListaDePaises.java
@@ -4,6 +4,7 @@ package Atividades_ArrayList;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 /**
  *
@@ -16,13 +17,19 @@ public class AT8_ListaDePaises {
         
         ArrayList<String> paises = new ArrayList<>();
         Collections.addAll(paises, "Brasil", "Portugal", "EUA", "Canadá", "Rússia");
+        Scanner tc = new Scanner(System.in);
         
         System.out.println("Lista de países:");
+        System.out.println(paises);
         
-        for(String lista : paises){
+        for(int i = 0; i < paises.size();  i++){
             
+            System.out.println("Digite o nome de outros países: ");
+            String pais = tc.nextLine();
+            paises.add(pais);
+            i++;
+            System.out.println(paises);
             
-            System.out.println(lista);
             
         }
         
